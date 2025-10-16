616a293600de90d9c582902d9e28b60cae6909a7
Touka Ri <ritouka90@gmail.com>
2025-10-16 17:47:14 +0900

    feat: Use named routing

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "static_pages#home"
 
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  get "/help", to: "static_pages#help"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
 end
