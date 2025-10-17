f6700ca4c8cce2630b258846b8287c2835581b39
Touka Ri <ritouka90@gmail.com>
2025-10-17 18:02:15 +0900

    fix: Log out from another logged-in browser

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
   end
 
   def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    remember_digest && BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
   def forget
