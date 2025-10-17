bf71a3a7374983023af6e7d5c0a6a0be5b0dbf89
Touka Ri <ritouka90@gmail.com>
2025-10-17 18:16:41 +0900

    feat: Remember me

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,5 +18,19 @@ module ActiveSupport
     def is_logged_in?
       !session[:user_id].nil?
     end
+
+    def log_in_as(user)
+      session[:user_id] = user.id
+    end
+  end
+end
+
+module ActionDispatch
+  class IntegrationTest
+    def log_in_as(user, password: "password", remember_me: "1")
+      post login_path, params: { session: { email: user.email,
+                                            password: password,
+                                            remember_me: remember_me } }
+    end
   end
 end
