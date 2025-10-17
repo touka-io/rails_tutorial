97fde9cfeac7a72f506bb6ba6b91def08dc3ba67
Touka Ri <ritouka90@gmail.com>
2025-10-17 16:57:57 +0900

    feat: Log in after user signed up

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,5 +14,9 @@ module ActiveSupport
 
     # Add more helper methods to be used by all tests here...
     include ApplicationHelper
+
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
   end
 end
