7e82202b5385f18bdeda8371fea28253530baec1
Touka Ri <ritouka90@gmail.com>
2025-10-17 15:59:31 +0900

    feat: Log in with valid info

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # redirect
+    if user&.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination"
       render "new", status: :unprocessable_entity
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,4 @@ import "@popperjs/core"
 import "bootstrap"
 
 import "controllers"
+import "custom/menu"
