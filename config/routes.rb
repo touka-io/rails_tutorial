db4dcb1bf26f2999ea16c8afb737ad38903a4018
Touka Ri <ritouka90@gmail.com>
2025-10-17 15:23:07 +0900

    feat: Log in with invalid info

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "users/new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -19,4 +18,8 @@ Rails.application.routes.draw do
   get "/signup", to: "users#new"
 
   resources :users
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
