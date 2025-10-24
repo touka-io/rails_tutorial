9899eb7d717ddf9e0841680e6d0842855b8a4dd1
Touka Ri <ritouka90@gmail.com>
2025-10-24 12:58:35 +0900

    feat: Handle user edit success case

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
                     uniqueness: true
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
