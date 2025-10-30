b02ce2d25185faa2b95c729b581d8c357cb01aa8
Touka Ri <ritouka90@gmail.com>
2025-10-30 17:43:11 +0900

    feat: Create micropost

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   include SessionsHelper
+
+  private
+
+  def logged_in_user
+    unless logged_in?
+    store_location
+    flash[:danger] = "Please log in."
+    redirect_to login_url, status: :see_other
+    end
+  end
 end
